5358a0cb5fba4ea4e4cf687a5cc0ec837256c718
meitali133 <meitali133@gmail.com>
2015-04-14 17:53:46 +0300

    עדכון מיטל
    מציג את הקורסים
    לא ראיתי איך מתבצעת ההעברה מעמוד HOME לשלי

--- FinalProject/WebServer/SearchResultsPage/SearchResultPageJS.js
+++ FinalProject/WebServer/SearchResultsPage/SearchResultPageJS.js
@@ -1,24 +1,29 @@
 ﻿
 $(document).ready(function () { ShowResults() });
 
-    function ShowResults() {
-        var uri;
-        //לבדוק מה נבחר, קורסים או מרצים
-        uri = '/api/SearchResult/GetCourses';
-        var arrayResult = [];
-        var courseName = "Math"; //צריך לקחת את השם שקיבלתתי בעמוד הקודם
+function ShowResults() {
+    var uri;
+    //לבדוק מה נבחר, קורסים או מרצים
+    uri = '/api/SearchResult/GetCourses';
+    var arrayResult = [];
+    var courseName = "Mat"; //צריך לקחת את השם שקיבלתתי בעמוד הקודם
 
 
-        $.getJSON(uri)
-            .done(function (data) { //פה אני מקבלת בדטה את כל המרצים או קורסים
-                var j = 0;
-                for (i in data) {
-                    if (courseName == data[i].Name || courseName.indexOf(data[i].Name, 0) > -1) {
-                        arrayResult.push(data[i].Name);
+    $.getJSON(uri)
+        .done(function (data) { //פה אני מקבלת בדטה את כל המרצים או קורסים
+            var j = 0;
+            for (i in data) {
+                if (courseName == data[i] || courseName.indexOf(data[i], 0) <= -1) {
+                    arrayResult.push(data[i]);
 
-                        document.write(arrayResult[j].toString());
-                        j++;
-                    }
+                    document.write(arrayResult[j].toString());
+                    j++;
+
+
                 }
-            });
-    };
+            }
+            if (arrayResult.length == 0) {
+                document.write("No matches found");
+            }
+        });
+};
