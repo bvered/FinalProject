d32731a2b237ebff5300bf03f27cf14ae134652f
meitali133 <meitali133@gmail.com>
2015-08-09 19:36:08 +0300

    update
    -need to save the extention of the file, in file upload to know what the
    kind of the file to open
    -need to figure out how to restore the the content of the real file from
    the array byte.

--- FinalProject/WebServer/Controllers/GetFileController.cs
+++ FinalProject/WebServer/Controllers/GetFileController.cs
@@ -29,7 +29,7 @@ namespace WebServer.Controllers
                         Guid syllabusId = CourseSemester.Syllabus.Id;
                         string semester = CourseSemester.Syllabus.Semster.ToString();
                         int year = CourseSemester.Syllabus.Year;
-                        byte[] file = CourseSemester.Syllabus.File;
+                   //     byte[] file = CourseSemester.Syllabus.File;
                         string fileName = CourseSemester.Syllabus.FileName;
                         result.Add(new ResultSyllabus(syllabusId, semester, year,file, fileName));
                     }
@@ -40,22 +40,51 @@ namespace WebServer.Controllers
             }
            
         }
+        /*Todo : GradesDestribution*/
+        /*  [HttpGet]
+          [ActionName("GetGradesDestribution")]
+          public IList<ResultGrades> GetGradesDestribution([FromUri]string id)
+          {
 
+          }
+          */
+
+        /*
+          [ActionName("GetSpecificGradeDistribution")]
+        public byte[] GetSpecificGradeDistribution([FromUri]string id)
+        {
+         
+         }
+         */
+
+        [ActionName("GetSpecificSyllabus")]
+        public byte[] GetSpecificSyllabus([FromUri]string id)
+        {
+            using (var session = DBHelper.OpenSession())
+            {
+                var Syllabus = session.QueryOver<Syllabus>().List();
+                IList<ResultSyllabus> result = new List<ResultSyllabus>();
+
+                var requestedSyllabus = session.Load<Syllabus>(new Guid(id));
+
+                return requestedSyllabus.File;
+            }
+        }
 
         public class ResultSyllabus
         {
             public Guid Id { get; set; }
             public string Semester { get; set; }
             public int Year { get; set; }
-            public byte[] File { get; set; }
+          //  public byte[] File { get; set; }
             public string FileName { get; set; }
 
-            public ResultSyllabus(Guid id, string semester, int year, byte[] file, string fileName)
+            public ResultSyllabus(Guid id, string semester, int year/*, byte[] file*/, string fileName)
             {
                 Id = id;
                 Semester = semester;
                 Year = year;
-                File = file;
+//                File = file;
                 FileName = fileName; 
             }
         }
---
+++ FinalProject/WebServer/DownloadFile/DownloadFile.js
@@ -0,0 +1,66 @@
+﻿$(document).ready()
+{
+    downloadFile();
+}
+
+var uri;
+
+function downloadFile() {
+    var quertyString = getQuertyString();
+
+    if (quertyString['type'] = 'Syllabus') {
+        uri = "/api/GetFile/GetSpecificSyllabus/" + quertyString['id'];
+        var ajaxRequest = $.ajax({
+            type: "GET",
+            url: uri,
+            contentType: "application/json",
+
+            success: function (data)
+            {
+//                console.log('meit');
+                //need to save the extention of the file, in file upload to know what the kind of the file to open
+                //need to figure out how to restore the the content of the real file from the array byte.
+                download(data, "test.txt", "text/plain");
+
+            },
+            fail: function (data)
+            {
+                //   succeed = false;
+            },
+       });
+    }
+    else if (quertyString[type] = Grades) {
+
+
+    }
+}
+
+function download(strData, strFileName, strMimeType) {
+    var D = document, A = arguments, a = D.createElement("a"),
+         d = A[0], n = A[1], t = A[2] || "text/plain";
+
+    //build download link:
+    a.href = "data:" + strMimeType + "," + escape(strData);
+
+    if ('download' in a) {
+        a.setAttribute("download", n);
+        a.innerHTML = "downloading...";
+        D.body.appendChild(a);
+        setTimeout(function () {
+            var e = D.createEvent("MouseEvents");
+            e.initMouseEvent(
+                 "click", true, false, window, 0, 0, 0, 0, 0
+                 , false, false, false, false, 0, null
+            );
+            a.dispatchEvent(e);
+            D.body.removeChild(a);
+        }, 66);
+        return true;
+    };
+
+    var f = D.createElement("iframe");
+    D.body.appendChild(f);
+    f.src = "data:" + (A[2] ? A[2] : "application/octet-stream")/*+ (window.btoa?";base64"+(window.btoa?window.btoa:escape)(strData))*/;
+    setTimeout(function () { D.body.removeChild(f); }, 333);
+    return true;
+}
